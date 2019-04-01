17c2a2c54b98c3690b9c5ac44938bc0df683f7cf
Sven Stoll <s.stoll@posteo.de>
2019-04-02 00:21:40 +0200

    Implement basic functionality to traverse the Kave dataset.

--- src/main/java/ch/uzh/ifi/ase/csccrecommender/index/IndexCreatorVisitor.java
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/index/IndexCreatorVisitor.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.ase.csccrecommender.index;
 import cc.kave.commons.model.ssts.IStatement;
 import cc.kave.commons.model.ssts.blocks.*;
 import cc.kave.commons.model.ssts.declarations.*;
+import cc.kave.commons.model.ssts.expressions.ISimpleExpression;
 import cc.kave.commons.model.ssts.expressions.assignable.*;
 import cc.kave.commons.model.ssts.expressions.loopheader.ILoopHeaderBlockExpression;
 import cc.kave.commons.model.ssts.expressions.simple.IConstantValueExpression;
@@ -21,307 +22,352 @@ import java.util.List;
 //              insert 'if' and 'else' keywords when we visit an 'IIfElseBlock'.
 
 @SuppressWarnings({"squid:S1185", "squid:S1135", "squid:CommentedOutCodeLine"})
-public class IndexCreatorVisitor extends AbstractTraversingNodeVisitor<List<IStatement>, Void> {
+public class IndexCreatorVisitor extends AbstractTraversingNodeVisitor<List<Object>, Void> {
 
   private static Logger logger = LoggerFactory.getLogger(IndexCreatorVisitor.class);
 
-  protected void handleCache(IStatement statement, List<IStatement> cache) {
+  protected void handleCache(IStatement statement, List<Object> cache) {
     cache.add(statement);
   }
-
+  
   @Override
-  public Void visit(IInvocationExpression expr, List<IStatement> cache) {
-    logger.info("Items in cache since last invocation: {}", cache.size());
-    cache.clear();
-    return super.visit(expr, cache);
+  public Void visit(IDelegateDeclaration stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IMethodDeclaration decl, List<IStatement> cache) {
-    logger.info("Invoked method: {}", decl.getName().getName());
-    return super.visit(decl, cache);
+  public Void visit(IEventDeclaration stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IDoLoop block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IFieldDeclaration stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(ILockBlock stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IMethodDeclaration decl, List<Object> context) {
+    visit(decl.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IForLoop block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IPropertyDeclaration decl, List<Object> context) {
+    visit(decl.getGet(), context);
+    visit(decl.getSet(), context);
+    return null;
   }
 
   @Override
-  public Void visit(ITryBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IVariableDeclaration stmt, List<Object> context) {
+    stmt.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IAssignment stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IAssignment stmt, List<Object> context) {
+    stmt.getReference().accept(this, context);
+    stmt.getExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IWhileLoop block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IBreakStatement stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IUsingBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IContinueStatement stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IForEachLoop block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IEventSubscriptionStatement stmt, List<Object> context) {
+    stmt.getReference().accept(this, context);
+    stmt.getExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IIfElseBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IExpressionStatement stmt, List<Object> context) {
+    stmt.getExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(ISwitchBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IGotoStatement stmt, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IUnsafeBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(ILabelledStatement stmt, List<Object> context) {
+    stmt.getStatement().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IEventReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(IReturnStatement stmt, List<Object> context) {
+    stmt.getExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IFieldReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(IThrowStatement stmt, List<Object> context) {
+    stmt.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IGotoStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IDoLoop block, List<Object> context) {
+    block.getCondition().accept(this, context);
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IBreakStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IForEachLoop block, List<Object> context) {
+    block.getDeclaration().accept(this, context);
+    block.getLoopedReference().accept(this, context);
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(ICastExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IForLoop block, List<Object> context) {
+    visit(block.getInit(), context);
+    block.getCondition().accept(this, context);
+    visit(block.getStep(), context);
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IMethodReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(IIfElseBlock block, List<Object> context) {
+    block.getCondition().accept(this, context);
+    visit(block.getThen(), context);
+    visit(block.getElse(), context);
+    return null;
   }
 
   @Override
-  public Void visit(INullExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(ILockBlock stmt, List<Object> context) {
+    stmt.getReference().accept(this, context);
+    visit(stmt.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IThrowStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(ISwitchBlock block, List<Object> context) {
+    block.getReference().accept(this, context);
+    for (ICaseBlock cb : block.getSections()) {
+      cb.getLabel().accept(this, context);
+      visit(cb.getBody(), context);
+    }
+    visit(block.getDefaultSection(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IReturnStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(ITryBlock block, List<Object> context) {
+    context.add("try");
+    visit(block.getBody(), context);
+    for (ICatchBlock cb : block.getCatchBlocks()) {
+      context.add("catch");
+      context.add(cb.getKind().getDeclaringClass().getName());
+      visit(cb.getBody(), context);
+    }
+    if (!block.getFinally().isEmpty()){
+      context.add("finally");
+    }
+    visit(block.getFinally(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IUnaryExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IUncheckedBlock block, List<Object> context) {
+    context.add("unchecked");
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IUncheckedBlock block, List<IStatement> cache) {
-    handleCache(block, cache);
-    return super.visit(block, cache);
+  public Void visit(IUnsafeBlock block, List<Object> context) {
+    context.add("unsafe");
+    // TODO svstoll: why no getBody()?
+    return null;
   }
 
   @Override
-  public Void visit(IUnknownReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(IUsingBlock block, List<Object> context) {
+    context.add("using");
+    context.add(block.getReference());
+//    block.getReference().accept(this, context);
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IBinaryExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IWhileLoop block, List<Object> context) {
+    context.add("while");
+    context.add(block.getCondition());
+    block.getCondition().accept(this, context);
+    visit(block.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IEventDeclaration stmt, List<IStatement> cache) {
-    //handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(ICompletionExpression entity, List<Object> context) {
+    // TODO sst: add something to the cache?
+    if (entity.getVariableReference() != null) {
+      entity.getVariableReference().accept(this, context);
+    }
+    return null;
   }
 
   @Override
-  public Void visit(IFieldDeclaration stmt, List<IStatement> cache) {
-    //handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IComposedExpression expr, List<Object> context) {
+    for (IVariableReference varRef : expr.getReferences()) {
+      varRef.accept(this, context);
+    }
+    return null;
   }
 
   @Override
-  public Void visit(IIfElseExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IIfElseExpression expr, List<Object> context) {
+    expr.getCondition().accept(this, context);
+    context.add("if");
+    expr.getThenExpression().accept(this, context);
+    context.add("else");
+    expr.getElseExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(ILambdaExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IInvocationExpression expr, List<Object> context) {
+    context.add(expr.getMethodName());
+    expr.getReference().accept(this, context);
+    for (ISimpleExpression p : expr.getParameters()) {
+      p.accept(this, context);
+    }
+    return null;
   }
 
   @Override
-  public Void visit(IPropertyReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(ILambdaExpression expr, List<Object> context) {
+    context.add(expr.getName().getExplicitMethodName()); // TODO svstoll: is this correct?
+    context.add(expr.getName().getReturnType()); // TODO svstoll: is this correct?
+    visit(expr.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IVariableReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(ILoopHeaderBlockExpression expr, List<Object> context) {
+    visit(expr.getBody(), context);
+    return null;
   }
 
   @Override
-  public Void visit(IContinueStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IConstantValueExpression expr, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(ILabelledStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(INullExpression expr, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IComposedExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IReferenceExpression expr, List<Object> context) {
+    expr.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IDelegateDeclaration stmt, List<IStatement> cache) {
-    //handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(ICastExpression expr, List<Object> context) {
+    context.add(expr.getTargetType());
+    context.add(expr.getOperator()); // TODO svstoll: what is this in C#?
+    expr.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IExpressionStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IIndexAccessExpression expr, List<Object> context) {
+    expr.getReference().accept(this, context);
+    for (ISimpleExpression idx : expr.getIndices()) {
+      idx.accept(this, context);
+    }
+    return null;
   }
 
   @Override
-  public Void visit(IIndexAccessReference ref, List<IStatement> cache) {
-    //handleCache(ref, cache);
-    return super.visit(ref, cache);
+  public Void visit(ITypeCheckExpression expr, List<Object> context) {
+    context.add(expr.getType());
+    expr.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IPropertyDeclaration decl, List<IStatement> cache) {
-    //handleCache(decl, cache);
-    return super.visit(decl, cache);
+  public Void visit(IBinaryExpression expr, List<Object> context) {
+    expr.getLeftOperand().accept(this, context);
+    expr.getRightOperand().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IReferenceExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IUnaryExpression expr, List<Object> context) {
+    expr.getOperand().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(ITypeCheckExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IEventReference ref, List<Object> context) {
+    ref.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IVariableDeclaration stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IFieldReference ref, List<Object> context) {
+    ref.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IIndexAccessExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IMethodReference ref, List<Object> context) {
+    context.add(ref.getMethodName());
+    ref.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(ICompletionExpression entity, List<IStatement> cache) {
-    //handleCache(entity, cache);
-    return super.visit(entity, cache);
+  public Void visit(IPropertyReference ref, List<Object> context) {
+    ref.getReference().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IConstantValueExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IVariableReference ref, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IUnknownStatement unknownStmt, List<IStatement> cache) {
-    handleCache(unknownStmt, cache);
-    return super.visit(unknownStmt, cache);
+  public Void visit(IIndexAccessReference ref, List<Object> context) {
+    ref.getExpression().accept(this, context);
+    return null;
   }
 
   @Override
-  public Void visit(IUnknownExpression unknownExpr, List<IStatement> cache) {
-    //handleCache(unknownExpr, cache);
-    return super.visit(unknownExpr, cache);
+  public Void visit(IUnknownReference ref, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(ILoopHeaderBlockExpression expr, List<IStatement> cache) {
-    //handleCache(expr, cache);
-    return super.visit(expr, cache);
+  public Void visit(IUnknownExpression unknownExpr, List<Object> context) {
+    return null;
   }
 
   @Override
-  public Void visit(IEventSubscriptionStatement stmt, List<IStatement> cache) {
-    handleCache(stmt, cache);
-    return super.visit(stmt, cache);
+  public Void visit(IUnknownStatement unknownStmt, List<Object> context) {
+    return null;
   }
 }
 
