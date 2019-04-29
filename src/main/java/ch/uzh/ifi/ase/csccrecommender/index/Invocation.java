d31c3d858b3808841cef4e9cce6533d4e1f3a3a3
Chad <347181599@qq.com>
2019-04-30 00:36:17 +0200

    29.04 jin

--- src/main/java/ch/uzh/ifi/ase/csccrecommender/index/Invocation.java
+++ src/main/java/ch/uzh/ifi/ase/csccrecommender/index/Invocation.java
@@ -3,10 +3,12 @@ package ch.uzh.ifi.ase.csccrecommender.index;
 import java.util.List;
 
 public class Invocation {
-
+	// TODO: tokensBeforeInvocation can represent overall context, but we also need linecontext, 
+	// 	which should be separated from tokensBeforeInvocation.
   private List<String> tokensBeforeInvocation;
   private String type;
   private String methodName;
+  	// TODO: create simhash(specifically, Jenkin hash function) for each document(invocation)
 
   public Invocation(List<String> tokensBeforeInvocation, String type, String methodName) {
     this.tokensBeforeInvocation = tokensBeforeInvocation;
